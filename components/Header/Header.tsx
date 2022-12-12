cc7b2ffac75cbd7735add94f3ab4f762177983ba
BeWoler <bewoler.del@gmail.com>
2022-12-12 23:50:17 +0300

    fix

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,7 +1,7 @@
 import { Box, Flex } from "@chakra-ui/react";
 import { useCookies } from "react-cookie";
 import { useAppSelector } from "../../redux/hooks/redux";
-import Button from "../common/Buttons/Button";
+import Button from "../common/Button/Button";
 
 const Header = () => {
   const [cookies, setCookie] = useCookies(['language'])
