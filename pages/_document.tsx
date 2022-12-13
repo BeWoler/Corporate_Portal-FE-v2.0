baa4c7682ee2a2e73d6489d1142ee623aac84cad
BeWoler <bewoler.del@gmail.com>
2022-12-13 18:02:26 +0300

    implement user store & changed header

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -30,10 +30,9 @@ class MyDocument extends Document {
         <Head>
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
-          <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet" />
-          <meta
-            name="viewport"
-            content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"
+          <link
+            href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap"
+            rel="stylesheet"
           />
           <meta name="contract-adress" content={contractAdress} />
           <meta name="referrer" content="strict-origin-when-cross-origin" />
---
+++ redux/services/user.slice.ts
@@ -0,0 +1,28 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { User } from "../models/user";
+
+export interface UserState {
+  value: User
+}
+
+const initialState: UserState = {
+  value: {
+    id: '',
+    email: '',
+    username: '',
+  }
+}
+
+export const userSlice = createSlice({
+  name: 'userData',
+  initialState,
+  reducers: {
+    userData: (state: UserState, action: PayloadAction<User>) => {
+      state.value = action.payload
+    }
+  }
+})
+
+export const { userData } = userSlice.actions;
+
+export default userSlice.reducer
